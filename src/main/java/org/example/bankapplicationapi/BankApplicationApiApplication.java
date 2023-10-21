fb595611dc1442289a2be307a65cdee25f0f9268
kaioa1meida <kaiohsilva04@gmail.com>
2023-10-21 13:14:08 -0300

    Initial commit

---
+++ src/main/java/org/example/bankapplicationapi/BankApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankapplicationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bankapplicationapi/BankApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankapplicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
